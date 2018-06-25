51bc6ed521662d2ac2739368d64d0be738c5c5c0
Edward C. Lim <mata1138@gmail.com>
2018-06-25 18:16:27 +0900

    Title scene layout and transitions
    Title scene logo and menu system layout and transitions of the scene

---
+++ Assets/Scripts/titleSceneAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titleSceneAnimation : MonoBehaviour {
+
+	Animator titleLogoAnim;
+	Animator mainMenusAnim;
+
+	public bool isTitleSceneLoaded = false;
+	public bool continueRequested = false;
+	public bool optionsRequested = false;
+
+	void Start () {
+		titleLogoAnim = GameObject.Find("TitleLogo").GetComponent<Animator>();
+		mainMenusAnim = GameObject.Find("MainMenus").GetComponent<Animator>();
+	}
+
+	void Update () {
+		// Triggering title logo and main menus animation and transition
+		if (isTitleSceneLoaded) {
+			titleLogoAnim.SetTrigger ("titleSceneLoaded");
+			mainMenusAnim.SetTrigger ("titleSceneLoaded");
+		}
+
+		if (isTitleSceneLoaded && continueRequested) {
+			titleLogoAnim.SetBool ("isContinueRequested", true);
+			mainMenusAnim.SetBool ("isContinueRequested", true);
+		} else if (isTitleSceneLoaded && !continueRequested) {
+			titleLogoAnim.SetBool ("isContinueRequested", false);
+			mainMenusAnim.SetBool ("isContinueRequested", false);
+		}
+
+		if (isTitleSceneLoaded && optionsRequested) {
+			titleLogoAnim.SetBool ("isOptionsRequested", true);
+			mainMenusAnim.SetBool ("isOptionsRequested", true);
+		} else if (isTitleSceneLoaded && !optionsRequested) {
+			titleLogoAnim.SetBool ("isOptionsRequested", false);
+			mainMenusAnim.SetBool ("isOptionsRequested", false);
+		}
+	}
+}
---
+++ Assets/Scripts/titleSceneManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleSceneManager : MonoBehaviour {
+
+	titleSceneAnimation titleSceneAnim;
+
+	void Start () {
+		titleSceneAnim = GetComponent<titleSceneAnimation>();
+	}
+
+	void Update () {
+		
+	}
+
+	// Title scene level manager methods
+	public void LoadLevel (string name)	{
+		SceneManager.LoadScene(name);
+	}
+
+	public void OpenLevelSelector () {
+		titleSceneAnim.continueRequested = true;
+	}
+
+	public void CloseLevelSelector () {
+		titleSceneAnim.continueRequested = false;
+	}
+
+	public void OpenOptions () {
+		titleSceneAnim.optionsRequested = true;
+	}
+
+	public void CloseOptions () {
+		titleSceneAnim.optionsRequested = false;
+	}
+
+	public void QuitRequest () {
+		Application.Quit();
+	}
+
+}
