d730d1f1f1eacd56242431d9290d2c07ad305473
Rajeev Mukundan <rajeev90km@gmail.com>
2018-08-26 01:05:24 -0500

    Vomit ZOmbie with placeholder FX added

---
+++ Assets/Code/AI/ZomzV2/ZombieVomit.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieVomit : ZombieBase 
+{
+    [Header("Vomit Zombie Config")]
+    [SerializeField]
+    private Transform _fxParent;
+
+    [SerializeField]
+    private Transform _vomitSpawnPosition;
+
+    [SerializeField]
+    private GameObject _vomitFXPrefab;
+
+    private int _enemyLayerMask;
+    private int _playerLayerMask;
+
+    GameObject _vomitFX;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _enemyLayerMask = (1 << LayerMask.NameToLayer("Enemy"));
+        _playerLayerMask = (1 << LayerMask.NameToLayer("Player"));
+    }
+
+    public override IEnumerator Attack()
+    {
+        if (IsAlive && !IsAttacking)
+        { 
+            if(!IsBeingControlled)
+                transform.LookAt(_player.transform);
+
+            if (IsBeingControlled)
+                _animator.SetTrigger("attack");
+
+            IsAttacking = true;
+
+            yield return new WaitForSeconds(0.8f);
+            if (_vomitFXPrefab != null)
+            {
+                _vomitFX = Instantiate(_vomitFXPrefab, _fxParent);
+                _vomitFX.transform.position = _vomitSpawnPosition.position;
+            }
+
+            yield return new WaitForSeconds(CharacterStats.AttackRate - 0.8f);
+
+            IsAttacking = false;
+        }
+
+        yield return null;
+    }
+}
---
+++ Assets/Code/Misc/ParticleHurt.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleHurt : MonoBehaviour {
+
+    [SerializeField]
+    private float _perParticleHurtAmount = 10f;
+
+    [SerializeField]
+    private int _hurtThreshold = 100;
+
+    int particleCount;
+
+	private void OnParticleCollision(GameObject other)
+	{
+        if(other.CompareTag("ParticleHurt"))
+        {
+            particleCount += 1;
+
+            if(particleCount == _hurtThreshold)
+            {
+                StartHurting();
+            }
+        }
+	}
+
+    private void StartHurting()
+    {
+        particleCount = 0;
+
+        if(gameObject.CompareTag("Player"))
+        {
+            CharacterControls player = GetComponent<CharacterControls>();
+
+            if (player && player.IsAlive)
+                player.StartCoroutine(player.Hurt(_perParticleHurtAmount));
+        }
+        else if(gameObject.CompareTag("Enemy"))
+        {
+            ZombieBase zombieBase = GetComponent<ZombieBase>();
+
+            if (zombieBase && zombieBase.IsAlive && !zombieBase.IsHurting)
+                zombieBase.StartCoroutine(zombieBase.Hurt(_perParticleHurtAmount));
+        }
+    }
+}
